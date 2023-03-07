317eb975f66c37f1199ce2f77366773cd432809d
SaraMathews <sara.malmstedt@gmail.com>
2023-03-07 12:31:02 +0000

    Add small changes to script.js modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,15 @@
-// Get the modal
-var modal = document.getElementById("rules-modal");
+//Open modal when rules btn is clicked
+// Gets id's and classes from the dom
+let modal = document.getElementById("rules-modal");
+let btn = document.getElementById("myBtn");
+let span = document.getElementsByClassName("close")[0];
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
+// Opens the modal when the user clicks the rules btn
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+// Closes the modal when user clicks on <span> (x)
 span.onclick = function() {
   modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
 }
