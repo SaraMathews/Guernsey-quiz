4d9a3d250c9d9ec1a2890b0348abda99849a1c5d
SaraMathews <sara.malmstedt@gmail.com>
2023-03-08 07:58:01 +0000

    Add style to modal in style.css

---
+++ assets/js/script-modal.js
@@ -0,0 +1,15 @@
+//Open modal when rules btn is clicked
+// Gets id's and classes from the dom
+let modal = document.getElementById("rules-modal");
+let btn = document.getElementById("myBtn");
+let span = document.getElementsByClassName("close")[0];
+
+// Opens the modal when the user clicks the rules btn
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+
+// Closes the modal when user clicks on <span> (x)
+span.onclick = function () {
+  modal.style.display = "none";
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -200,19 +200,3 @@ function endQuiz() {
   let src = document.getElementById("img");
   src.appendChild(img)
 }
-
-//Open modal when rules btn is clicked
-// Gets id's and classes from the dom
-let modal = document.getElementById("rules-modal");
-let btn = document.getElementById("myBtn");
-let span = document.getElementsByClassName("close")[0];
-
-// Opens the modal when the user clicks the rules btn
-btn.onclick = function () {
-  modal.style.display = "block";
-}
-
-// Closes the modal when user clicks on <span> (x)
-span.onclick = function () {
-  modal.style.display = "none";
-}
